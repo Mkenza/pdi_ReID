1f6de115459bd2a09e4a3ae8a7ce6a8336f41c32
Kenza <knzmakhlouf@gmail.com>
2023-03-06 14:54:50 +0100

    corrected path in feature extractor file

--- deep_sort/deep/feature_extractor.py
+++ deep_sort/deep/feature_extractor.py
@@ -5,27 +5,30 @@ import cv2
 import logging
 from .reid_model import ReIdModel
 from .model import Net
+
 # from fastreid.config import get_cfg
 # from fastreid.engine import DefaultTrainer
 # from fastreid.utils.checkpoint import Checkpointer
+
 
 class Extractor(object):
     def __init__(self, model_path, opt, use_cuda=True):
         self.net = ReIdModel(opt)
         self.device = "cuda" if torch.cuda.is_available() and use_cuda else "cpu"
-        # state_dict = torch.load(model_path, map_location=lambda storage, loc: storage)['net_dict']
-        state_dict = torch.load(model_path, map_location=lambda storage, loc: storage)
-        self.net.load_state_dict(torch.load(opt.unet_path,  map_location=torch.device('cpu')))
+
+        self.net.load_state_dict(
+            torch.load(opt.reidmodel_path, map_location=lambda storage, loc: storage)
+        )
         logger = logging.getLogger("root.tracker")
-        logger.info("Loading weights from {}... Done!".format(model_path))
+        logger.info("Loading weights from {}... Done!".format(opt.reidmodel_path))
         self.net.to(self.device)
         self.size = (64, 128)
-        self.norm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-        ])
-        
-
+        self.norm = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ]
+        )
 
     def _preprocess(self, im_crops):
         """
@@ -36,12 +39,48 @@ class Extractor(object):
             3. to torch Tensor
             4. normalize
         """
-        def _resize(im, size):
-            return cv2.resize(im.astype(np.float32)/255., size)
 
-        im_batch = torch.cat([self.norm(_resize(im, self.size)).unsqueeze(0) for im in im_crops], dim=0).float()
+        def _resize(im, size):
+            return cv2.resize(im.astype(np.float32) / 255.0, size)
+
+        im_batch = torch.cat(
+            [self.norm(_resize(im, self.size)).unsqueeze(0) for im in im_crops], dim=0
+        ).float()
         return im_batch
 
+    def __call__(self, im_crops):
+        im_batch = self._preprocess(im_crops)
+        with torch.no_grad():
+            im_batch = im_batch.to(self.device)
+            features = self.net(im_batch)
+        return features.cpu().numpy()
+
+    # class FastReIDExtractor(object):
+    #     def __init__(self, model_config, model_path, use_cuda=True):
+    #         cfg = get_cfg()
+    #         cfg.merge_from_file(model_config)
+    #         cfg.MODEL.BACKBONE.PRETRAIN = False
+    #         self.net = DefaultTrainer.build_model(cfg)
+    #         self.device = "cuda" if torch.cuda.is_available() and use_cuda else "cpu"
+
+    #         Checkpointer(self.net).load(model_path)
+    #         logger = logging.getLogger("root.tracker")
+    #         logger.info("Loading weights from {}... Done!".format(model_path))
+    #         self.net.to(self.device)
+    #         self.net.eval()
+    #         height, width = cfg.INPUT.SIZE_TEST
+    #         self.size = (width, height)
+    #         self.norm = transforms.Compose([
+    #             transforms.ToTensor(),
+    #             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    #         ])
+
+    #     def _preprocess(self, im_crops):
+    #         def _resize(im, size):
+    #             return cv2.resize(im.astype(np.float32)/255., size)
+
+    #         im_batch = torch.cat([self.norm(_resize(im, self.size)).unsqueeze(0) for im in im_crops], dim=0).float()
+    #         return im_batch
 
     def __call__(self, im_crops):
         im_batch = self._preprocess(im_crops)
@@ -50,47 +89,9 @@ class Extractor(object):
             features = self.net(im_batch)
         return features.cpu().numpy()
 
-# class FastReIDExtractor(object):
-#     def __init__(self, model_config, model_path, use_cuda=True):
-#         cfg = get_cfg()
-#         cfg.merge_from_file(model_config)
-#         cfg.MODEL.BACKBONE.PRETRAIN = False
-#         self.net = DefaultTrainer.build_model(cfg)
-#         self.device = "cuda" if torch.cuda.is_available() and use_cuda else "cpu"
 
-#         Checkpointer(self.net).load(model_path)
-#         logger = logging.getLogger("root.tracker")
-#         logger.info("Loading weights from {}... Done!".format(model_path))
-#         self.net.to(self.device)
-#         self.net.eval()
-#         height, width = cfg.INPUT.SIZE_TEST
-#         self.size = (width, height)
-#         self.norm = transforms.Compose([
-#             transforms.ToTensor(),
-#             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-#         ])
-
-    
-#     def _preprocess(self, im_crops):
-#         def _resize(im, size):
-#             return cv2.resize(im.astype(np.float32)/255., size)
-
-#         im_batch = torch.cat([self.norm(_resize(im, self.size)).unsqueeze(0) for im in im_crops], dim=0).float()
-#         return im_batch
-
-
-    def __call__(self, im_crops):
-        im_batch = self._preprocess(im_crops)
-        with torch.no_grad():
-            im_batch = im_batch.to(self.device)
-            features = self.net(im_batch)
-        return features.cpu().numpy()
-
-
-
-if __name__ == '__main__':
-    img = cv2.imread("demo.jpg")[:,:,(2,1,0)]
+if __name__ == "__main__":
+    img = cv2.imread("demo.jpg")[:, :, (2, 1, 0)]
     extr = Extractor("model_unet.PTH")
     feature = extr(img)
     print(feature.shape)
-
